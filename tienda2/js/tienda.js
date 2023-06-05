12ef59ee172234573cd8909ad6a48cc864653710
tiagoraminelli <108493070+tiagoraminelli@users.noreply.github.com>
2023-06-04 23:49:41 -0300

    Add files via upload

---
+++ tienda2/js/tienda.js
@@ -0,0 +1,3 @@
+const btnCart = document.querySelector('.container-icon')
+const containerCartProducts = document.querySelector('.container-cart-products')
+btnCart.addEventListener('click',()=>{containerCartProducts.classList.toggle('hidden-cart')})
